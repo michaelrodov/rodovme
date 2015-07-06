3e8d74be71deb4ae9afe4120087d0aa94a0a9812
Michael Rodov <rodov@hp.com>
2015-07-06 22:06:29 +0300

    UI
     - replaced svg by svg files binded by ng-img

--- ga/js/index.js
+++ ga/js/index.js
@@ -5,14 +5,11 @@ var dashApp = angular.module('dashApp', ['ngMaterial']);
 dashApp.controller('dashCtrl', function($scope, $http, $interval) {
     //init
     $scope.data = [];
-    $scope.data.push({name:"agm", users: getRTUsers(), title: "Agile Manager"});
-    $scope.data.push({name:"saw", users: getRTUsers(), title: "Service Anywhere"});
-    $scope.data.push({name:"srl", users: getRTUsers(), title: "StormRunner Load"});
-    $scope.data.push({name:"ma",
-                        users: getRTUsers(),
-                        title: "MyAccount",
-                        logosvg: "M141.015,71.998c-3.824,0-7.111,2.267-8.615,5.524h-19.175c-1.621,0-3.08,0.985-3.684,2.49l-9.561,23.811  l-9.2-62.327c-0.263-1.779-1.689-3.16-3.477-3.364c-1.789-0.204-3.488,0.819-4.146,2.493L69.117,76.388H10.491v7.94h61.333  c1.633,0,3.099-0.999,3.695-2.519L85.102,57.4l9.167,62.103c0.262,1.773,1.681,3.152,3.461,3.363  c0.157,0.018,0.313,0.027,0.468,0.027c1.604,0,3.072-0.973,3.683-2.49l14.028-34.941h16.491c1.504,3.258,4.791,5.523,8.615,5.523  c5.243,0,9.494-4.25,9.494-9.493S146.258,71.998,141.015,71.998z"});
-    $scope.data.push({name:"appm", users: getRTUsers(), title: "AppPulse Mobile"});
+    $scope.data.push({name:"agm", users: getRTUsers(), title: "Agile Manager", logo: "img/agm.svg"});
+    $scope.data.push({name:"saw", users: getRTUsers(), title: "Service Anywhere", logo: "img/saw.svg"});
+    $scope.data.push({name:"srl", users: getRTUsers(), title: "StormRunner Load", logo: "img/srl.svg"});
+    $scope.data.push({name:"ma",   users: getRTUsers(),title: "MyAccount", logo: "img/hp.black.png"});
+    $scope.data.push({name:"appm", users: getRTUsers(), title: "AppPulse Mobile", logo: "img/appm.svg"});
 
     function authenticate(){
         var auth = {
