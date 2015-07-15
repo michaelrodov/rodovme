b9e40b6f008e760cdf960c8bf8024bcdfb391d62
amir.gabay@hp.com <amir.gabay@hp.com>
2015-07-15 19:39:10 +0300

    Pull 'Key' from url

--- ga/js/auth.js
+++ ga/js/auth.js
@@ -1,5 +1,5 @@
       var clientId = '331390481649-7c33q2tflt39r9asunr1j12775tcorvb.apps.googleusercontent.com';
-      var apiKey = 'AIzaSyCIoKf8foACQSD-7-BN94VcKGGYmVIqWWU';
+    //  var apiKey = 'AIzaSyCIoKf8foACQSD-7-BN94VcKGGYmVIqWWU';
       var scopes = 'https://www.googleapis.com/auth/analytics';
 	  var maGaId = 'ga:81002014';
 	  var sawGaId = 'ga:85904512';
@@ -7,6 +7,34 @@
 	  var apmGaId = 'ga:93588054';
      
      
+
+var apiKey = function () {
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("?");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+  for(Key in query_string) {
+    if(query_string.hasOwnProperty(Key)) {
+        var query_string_value = query_string[Key];
+    }
+}
+    return query_string_value;
+}();
+
+	 
 	 function handleClientLoad() {
         gapi.client.setApiKey(apiKey);
         window.setTimeout(checkAuth,100);
