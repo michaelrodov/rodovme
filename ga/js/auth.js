b5d5cc60fea8c165962fbee1ac9d023a7b8a4226
Michael Rodov <rodov@hp.com>
2015-07-15 21:15:32 +0300

    API key as parameter
     - code review comments

--- ga/js/auth.js
+++ ga/js/auth.js
@@ -7,13 +7,16 @@
 	  var apmGaId = 'ga:93588054';
      
      
-
+//[MR] it would be better to create a function that returns a value
+// you don't use this function more then once 
 var apiKey = function () {
   var query_string = {};
   var query = window.location.search.substring(1);
-  var vars = query.split("?");
+  var vars = query.split("?"); //[MR] why you split by "?", it splits the parameters part and server part, but query only has params
+								//[MR]if anything you should be splitting by "&" i.e. some.com/test?v1=444&v2=555 
   for (var i=0;i<vars.length;i++) {
     var pair = vars[i].split("=");
+	//[MR] I wouldnt do a loop: just take the query var and run something like query.match(/key=[^&]*/), which means search for "key=" and subsequent char after it except "&" 
         // If first entry with this name
     if (typeof query_string[pair[0]] === "undefined") {
       query_string[pair[0]] = decodeURIComponent(pair[1]);
@@ -23,7 +26,7 @@ var apiKey = function () {
       query_string[pair[0]] = arr;
         // If third or later entry with this name
     } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+      query_string[pair[0]].push(decodeURIComponent(pair[1])); //[MR] just return the value, don't save it in 
     }
   } 
   for(Key in query_string) {
