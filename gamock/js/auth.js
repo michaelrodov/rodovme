5fcf0fede88e1722d0a86316ffea15af6779a8f4
Michael Rodov <rodov@hp.com>
2015-07-22 00:00:11 +0300

    GA MOCK
     - initial

---
+++ gamock/js/auth.js
@@ -0,0 +1,39 @@
+      var clientId = '331390481649-7c33q2tflt39r9asunr1j12775tcorvb.apps.googleusercontent.com';
+    //  var apiKey = 'AIzaSyCIoKf8foACQSD-7-BN94VcKGGYmVIqWWU';
+      var scopes = 'https://www.googleapis.com/auth/analytics';
+	  var maGaId = 'ga:81002014';
+	  var sawGaId = 'ga:85904512';
+	  var srlGaId = 'ga:87422827';
+	  var apmGaId = 'ga:93588054';
+     
+var apiKey = location.search
+apiKey = apiKey.substring(apiKey.indexOf("=") + 1);
+
+	 function handleClientLoad() {
+        gapi.client.setApiKey(apiKey);
+        window.setTimeout(checkAuth,100);
+        window.setInterval(checkAuth,2700000)
+      }
+	  
+      function checkAuth() {
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+      }
+	  
+      function handleAuthResult(authResult) {
+        var authorizeButton = document.getElementById('authorize-button');
+        var progressCircular = document.getElementById('progress-circular');
+        if (authResult && !authResult.error) {
+          authorizeButton.style.visibility = 'hidden';
+          progressCircular.style.visibility = '';
+		  //authentication finished
+        } else {
+          authorizeButton.style.visibility = '';
+          authorizeButton.onclick = handleAuthClick;
+        }
+      }
+	  
+      function handleAuthClick(event) {
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+        return false;
+      }
+ 
---
+++ gamock/js/index.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Carlos on 29/06/2015.
+ *
+ */
+var dashApp = angular.module('dashApp', ['ngMaterial'])
+				.config(function($mdThemingProvider) {
+					  $mdThemingProvider.theme('default')
+						.primaryPalette('yellow')
+					});
+					
+dashApp.controller('dashCtrl', function($scope, $http, $interval) {
+	
+	var REFRESH_RATE_SEC = 10;
+	var AUTH_RATE = 300;
+	$scope.authenticateIn=300;
+	$scope.determinateValue = 0;
+	$scope.maxValue = REFRESH_RATE_SEC;
+    //init
+    $scope.data = [];
+    $scope.data.push({name:"agm", users: "0", title: "Agile Manager", logo: "img/agm.png",gaid: "ga:59215255", mn:40, mx:60});
+    $scope.data.push({name:"saw", users: "0", title: "Service Anywhere", logo: "img/saw.png",gaid: "ga:85914404", mn:250, mx:270});
+    $scope.data.push({name:"srl", users: "0", title: "StormRunner Load", logo: "img/srl.png",gaid: "ga:87422827", mn:0, mx:3});
+    $scope.data.push({name:"appm", users: "0", title: "AppPulse Mobile", logo: "img/appm.png",gaid: "ga:71897204", mn:3, mx:9});
+    $scope.data.push({name:"ma", users: "0", title: "MyAccount", logo: "img/grey-HP-logo.png",gaid: "ga:81002014", mn:60, mx:80});
+	 
+
+    $scope.refresh = function() {
+        for (var i = 0, len = $scope.data.length; i < len; i++) {
+            getRTUsers($scope.data[i]);
+        }
+    }
+	
+	/*refresh every second*/
+    $interval(function(){
+
+		if($scope.determinateValue>99){
+			$scope.determinateValue=0; 
+			$scope.refresh();
+		}
+		$scope.determinateValue += Math.round(100/REFRESH_RATE_SEC);
+		
+	}, 1000);
+
+	function getRTUsers(item) {
+			return item.mn + Math.round((item.mx-item.mn)*Math.random());
+		  }
+		  
+	$scope.setDataValueByGaid = function(gaid, users){
+		var i=0;
+		while($scope.data[i].gaid != gaid && i<$scope.data.length){
+			i++;
+		}
+		if(i<$scope.data.length){
+			$scope.data[i].users = users;
+		}
+	}	
+	
+
+
+
+
+});
